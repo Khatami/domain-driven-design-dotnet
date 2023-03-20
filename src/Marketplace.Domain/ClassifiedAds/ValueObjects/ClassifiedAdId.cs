d03292aef3f7b14e91fcd186c0eb00c694ab7ae9
hamedkhatami <hamedkhatami@outlook.com>
2023-03-20 12:53:26 +0330

    feat: add projects

---
+++ src/Marketplace.Domain/ClassifiedAds/ValueObjects/ClassifiedAdId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Marketplace.Domain.ClassifiedAds.ValueObjects
+{
+    public class ClassifiedAdId
+    {
+        private readonly Guid _value;
+
+        public ClassifiedAdId(Guid value)
+        {
+            if (value == default)
+                throw new ArgumentException("OwnerId must be set", nameof(value));
+
+            _value = value;
+        }
+    }
+}
---
+++ src/Marketplace.Domain/ClassifiedAds/ValueObjects/Money.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Marketplace.Domain.ClassifiedAds.ValueObjects
+{
+	public class Money : IEquatable<Money>
+	{
+		public decimal Amount { get; }
+
+		public Money(decimal amount)
+		{
+			Amount = amount;
+		}
+
+		public bool Equals(Money other)
+		{
+			if (ReferenceEquals(other, null)) { return false; }
+			if (ReferenceEquals(this, other)) { return true; }
+			if (this.Amount == other.Amount) { return true; }
+
+			return false;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj.GetType() != typeof(Money)) { return false; }
+
+			return Equals((Money)obj);
+		}
+
+		public override int GetHashCode() => Amount.GetHashCode();
+
+		public static bool operator == (Money left, Money right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator != (Money left, Money right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
