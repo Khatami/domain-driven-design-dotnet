0737d340bc34041afaf79524e1c48c855ecfc8c6
hamedkhatami <hamedkhatami@outlook.com>
2023-03-22 21:03:36 +0430

    feat: add some tests

--- src/Marketplace.Tests/ClassifiedAd/MoneyTest.cs
+++ src/Marketplace.Tests/ClassifiedAd/MoneyTest.cs
@@ -1,4 +1,5 @@
 ﻿using Marketplace.Domain.ClassifiedAd.DomainServices;
+using Marketplace.Domain.ClassifiedAd.Exceptions;
 using Marketplace.Domain.ClassifiedAd.ValueObjects;
 
 namespace Marketplace.Tests.ClassifiedAd
@@ -8,13 +9,31 @@ namespace Marketplace.Tests.ClassifiedAd
         private ICurrencyLookup _currencyLookup = new FakeCurrencyLookup();
 
         [Fact]
-        public void Money_objects_with_the_same_amount_should_be_equal()
+        public void Two_of_same_amount_should_be_equal()
         {
             var firstAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
             var secondAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
 
             Assert.Equal(firstAmount, secondAmount);
         }
+
+        [Fact]
+        public void Two_of_same_amount_but_differentCurrencies_should_not_eb_equal()
+        {
+			var firstAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
+			var secondAmount = Money.FromDecimal(5, "USD", _currencyLookup);
+
+			Assert.NotEqual(firstAmount, secondAmount);
+		}
+
+        [Fact]
+        public void FromString_and_fromDecimal_should_be_equal()
+        {
+			var firstAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
+			var secondAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
+
+			Assert.Equal(firstAmount, secondAmount);
+		}
 
         [Fact]
         public void Sum_of_money_gives_full_amount()
@@ -25,5 +44,41 @@ namespace Marketplace.Tests.ClassifiedAd
 
             Assert.Equal(firstAmount + secondAmount, thirdAmount);
         }
-    }
+
+        [Fact]
+        public void Unused_currency_should_not_be_allowed()
+        {
+            Assert.Throws<ArgumentException>(() => Money.FromDecimal(5, "DEM", _currencyLookup));
+        }
+
+		[Fact]
+		public void Unknwon_currency_should_not_be_allowed()
+		{
+			Assert.Throws<ArgumentException>(() => Money.FromDecimal(5, "WHEN", _currencyLookup));
+		}
+
+		[Fact]
+		public void Throw_when_too_many_decimal_places()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => Money.FromDecimal(5.123M, "EUR", _currencyLookup));
+		}
+
+        [Fact]
+        public void Throws_on_adding_different_currencies()
+        {
+            var firstAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
+			var secondAmount = Money.FromDecimal(5, "USD", _currencyLookup);
+
+            Assert.Throws<CurrencyMismatchException>(() => firstAmount + secondAmount);
+		}
+
+		[Fact]
+		public void Throws_on_subtracting_different_currencies()
+		{
+			var firstAmount = Money.FromDecimal(5, "EUR", _currencyLookup);
+			var secondAmount = Money.FromDecimal(5, "USD", _currencyLookup);
+
+			Assert.Throws<CurrencyMismatchException>(() => firstAmount - secondAmount);
+		}
+	}
 }
