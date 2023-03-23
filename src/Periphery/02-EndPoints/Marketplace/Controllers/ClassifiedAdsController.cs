c76180a648f82cbd72b72fd4bc6e631f1cc9f696
hamedkhatami <hamedkhatami@outlook.com>
2023-03-24 03:09:45 +0430

    feat: add the edge layer

---
+++ src/Periphery/02-EndPoints/Marketplace/Controllers/ClassifiedAdsController.cs
@@ -0,0 +1,18 @@
+﻿using Marketplace.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClassifiedAdsController : Controller
+	{
+		[HttpPost]
+		public async Task<IActionResult> Post(ClassifiedAds.V1.Create request)
+		{
+			// Handle the request here
+
+			return Ok();
+		}
+	}
+}
