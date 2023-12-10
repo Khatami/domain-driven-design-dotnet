d6294ab706f524cedccffc3ec2157464f841e793
hamedkhatami <hamedkhatami@outlook.com>
2023-12-10 14:59:46 +0330

    feat: add RavenDb Nuget To Queries.RavenDB

--- src/03-Infrastructure/Marketplace.Queries.RavenDB/ClassifiedAds/QueryHandlers/ClassifiedAdListItemQueryHandler.cs
+++ src/03-Infrastructure/Marketplace.Queries.RavenDB/ClassifiedAds/QueryHandlers/ClassifiedAdListItemQueryHandler.cs
@@ -1,12 +1,20 @@
 ﻿using Marketplace.Application.Infrastructure.Mediator;
 using Marketplace.Queries.Contracts.ClassifiedAds.QueryFilters;
 using Marketplace.Queries.Contracts.ClassifiedAds.QueryResults;
+using Raven.Client.Documents.Session;
 
 namespace Marketplace.Queries.RavenDB.ClassifiedAds.QueryHandlers
 {
 	internal class ClassifiedAdListItemQueryHandler : IQueryHandler<GetPublishedAdsQueryFilter, List<ClassifiedAdItem>>
     {
-        public async Task<List<ClassifiedAdItem>> Handle(GetPublishedAdsQueryFilter request, CancellationToken cancellationToken)
+        private readonly IAsyncDocumentSession _session;
+
+		public ClassifiedAdListItemQueryHandler(IAsyncDocumentSession session)
+		{
+			_session = session;
+		}
+
+		public async Task<List<ClassifiedAdItem>> Handle(GetPublishedAdsQueryFilter request, CancellationToken cancellationToken)
         {
             return new List<ClassifiedAdItem>();
         }
